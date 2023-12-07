0d7851bddd977d9948b61a4dc6ee8e9a2767c909
alexresiga <resiga.alex@gmail.com>
2023-12-07 02:23:19 +0200

    improve util function 🔧

--- src/utils.kt
+++ src/utils.kt
@@ -12,7 +12,7 @@ enum class Part {
 
 inline fun <reified T> T.println(): T = also { println(it) }
 
-inline fun day(block: (part: Part, input: List<String>) -> Any?) {
+inline fun day(both: Boolean = false, block: (part: Part, input: List<String>) -> Any?) {
     val input = Path("src/data.in").readLines()
-    Part.entries.forEach { block(it, input) }
+    Part.entries.forEach { block(it, input); if (!both) return }
 }
